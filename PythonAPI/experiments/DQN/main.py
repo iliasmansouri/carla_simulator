9acd12b1c674a72ccb7d2097296283e3104e2362
Ilias <ilias.mansouri@keemotion.com>
2020-12-29 13:54:57 +0100

    add fast dev run test

--- PythonAPI/experiments/DQN/main.py
+++ PythonAPI/experiments/DQN/main.py
@@ -1,5 +1,6 @@
 from dqn import DQN, Agent, ReplayBuffer, DQNLightning
 from world import World
+import pytorch_lightning as pl
 
 
 def test_world():
@@ -20,5 +21,16 @@ def test_agent():
     world.destroy_all()
 
 
+def test_dqn():
+    world = World()
+    model = DQNLightning(world)
+
+    trainer = pl.Trainer(gpus=1, fast_dev_run=True)
+    trainer.fit(model)
+
+    world.destroy_all()
+
+
 if __name__ == "__main__":
-    test_agent()
+    # test_agent()
+    test_dqn()
